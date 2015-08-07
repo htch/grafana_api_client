65993ccfd9bf085bd23d498481618e60a462b626
Pavel <htch.git@gmail.com>
2015-08-07 15:09:28 +0300

    making a better api

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='grafana_api_client',
-    version='0.1.2',
+    version='0.1.3',
     url='https://github.com/htch/grafana_api_client',
     description='Grafana API wrapper/Very basic Grafana API client library.',
     author='Pavel',
