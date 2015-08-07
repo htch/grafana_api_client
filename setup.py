ffd81124129262b113750f1669582dd64842d6dc
Pavel <htch.git@gmail.com>
2015-08-07 14:54:27 +0300

    fixing broken setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@
 from distutils.core import setup
 
 setup(name='grafana_api_client',
-    version='0.1',
+    version='0.1.1',
     url='https://github.com/htch/grafana_api_client',
     description='Grafana API wrapper/Very basic Grafana API client library.',
     author='Pavel',
     author_email='htch.git@gmail.com',
+    packages=["grafana_api_client"],
     requires=[
         "requests",
         "wsgiref",
